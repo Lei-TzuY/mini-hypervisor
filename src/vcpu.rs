bd6eeefc2075f5b34cea2eeb7592181c062ca07c
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-05 09:24:26 +0800

    Apply rustfmt module ordering

--- src/vcpu.rs
+++ src/vcpu.rs
@@ -18,8 +18,8 @@ pub use special_register_snapshot::{
     VcpuSpecialRegisterField, VcpuSpecialRegisterMismatch, VcpuSpecialRegisterSnapshot,
     VcpuSpecialRegisterSnapshotComparison,
 };
-mod special_register_restore;
 mod interrupt;
+mod special_register_restore;
 
 mod msr_readback;
 pub use msr_readback::{VcpuMsrValue, VcpuMsrValues};
