fcabd75bce442266de5b39a0115ad3e756c6142d
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-06 18:37:18 +0800

    Format AP local timer executable

--- src/bin/two-vcpu-ap-local-timer.rs
+++ src/bin/two-vcpu-ap-local-timer.rs
@@ -1,6 +1,6 @@
 use mini_hypervisor::portio::two_vcpu_ap_local_timer_fixture::{
-    run_two_vcpu_ap_local_timer, AP_LOCAL_TIMER_DIVIDE_CONFIGURATION,
-    AP_LOCAL_TIMER_INITIAL_COUNT, AP_LOCAL_TIMER_VECTOR,
+    run_two_vcpu_ap_local_timer, AP_LOCAL_TIMER_DIVIDE_CONFIGURATION, AP_LOCAL_TIMER_INITIAL_COUNT,
+    AP_LOCAL_TIMER_VECTOR,
 };
 use std::process::ExitCode;
 
@@ -14,7 +14,10 @@ fn main() -> ExitCode {
             );
             println!("AP local timer initial count: {AP_LOCAL_TIMER_INITIAL_COUNT:#x}");
             println!("AP local timer watchdog fired: {}", result.watchdog_fired());
-            println!("AP local timer initial MP state: {}", result.initial_ap_mp_state());
+            println!(
+                "AP local timer initial MP state: {}",
+                result.initial_ap_mp_state()
+            );
             println!(
                 "AP local timer startup: mp={} rip={:#x} cs={:#x} base={:#x}",
                 state.startup_mp_state(),
