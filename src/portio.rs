2f47a9302798cf27d07b6c57dc0d6d118fbcbf37
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-11 01:42:49 +0800

    Remove checkpoint debug-port tracing

--- src/portio.rs
+++ src/portio.rs
@@ -147,11 +147,7 @@ impl DebugPort {
                     });
                 }
 
-                let byte = io.output_data()[0];
-                self.bytes.push(byte);
-                if std::env::var_os("MINI_HYPERVISOR_TRACE_DEBUG_PORT").is_some() {
-                    eprintln!("debug-port trace: {byte:#04x}");
-                }
+                self.bytes.push(io.output_data()[0]);
                 Ok(PortIoService::Output)
             }
             PortIoDirection::In => Ok(PortIoService::Input(vec![self.input_byte])),
