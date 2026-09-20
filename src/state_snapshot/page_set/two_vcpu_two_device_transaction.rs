7a3b0621160d4b66909cb9d54656839ed8b03f66
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-21 04:12:27 +0800

    Wire pending completion replay proof

--- src/state_snapshot/page_set/two_vcpu_two_device_transaction.rs
+++ src/state_snapshot/page_set/two_vcpu_two_device_transaction.rs
@@ -561,5 +561,6 @@ mod two_vcpu_two_device_transaction_guest {
     use super::*;
     include!("two_vcpu_two_device_transaction_guest.rs");
     include!("restored_multi_producer_data_plane_replay.rs");
+    include!("pending_completion_token_replay.rs");
 }
 pub use two_vcpu_two_device_transaction_guest::*;
